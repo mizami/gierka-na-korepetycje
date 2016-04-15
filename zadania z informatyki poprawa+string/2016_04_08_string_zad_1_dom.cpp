01eb1ca57ba0b61d4ebeb0689331118afd70e44a
mizami <kopeknumerouno@gmail.com>
2016-04-16 01:57:00 +0200

    przepraszam żę tak mało...
    naprawde nei mam usprawiedliwiania....uzależnienie i lenistwo wzieło
    górę ale w końcu się wziełem za siebie

--- zadania z informatyki poprawa+string/2016_04_08_string_zad_1_dom.cpp
+++ zadania z informatyki poprawa+string/2016_04_08_string_zad_1_dom.cpp
@@ -4,7 +4,46 @@
 // osobna funkcja na sprawdzanie obecnoœci kropki w pobranym stringu
 // zak³adamy, ¿e w zdania w pliku s¹ zapisane poprawnie
 
-
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cstring>
 
+using namespace std;
+
+int main()
+{
+	int liczba_zdan=0,liczba_slow=0,srednia=0;
+	fstream zadanie;
+	zadanie.open("zaddom1.txt",ios::out);
+	if (plik.good())
+	{
+		while(!zadanie.eof())
+		{
+			if(liczba_zdan!=1)
+			{
+				liczba_slow++
+				if(zdanie==.)
+				{
+					liczba_zdan++;
+					cout<<"w zdaniu "<<liczba_zdan<<" jest "<<liczba_slow<<" slow."<<endl;
+					liczbaslow=0;
+				}
+			}
+			else
+			{
+				liczba_slow++
+				if(zdanie==.)
+				{
+					liczba_zdan++;
+					cout<<"w zdaniu "<<liczba_zdan<<" jest "<<liczba_slow<<" slow."<<endl;
+					liczba_slow=0;
+				}	
+			}
+		}
+	}
+		zadanie.close();
+		system ("PAUSE");
+		return 0;	
+}
