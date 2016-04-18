315f719551d49e177d5343eeb58a3bcb8d66bcdd
mizami <kopeknumerouno@gmail.com>
2016-04-19 00:33:55 +0200

    zrobiłem tylko tyle :(
    przepraszam że tak mało + 1 program dalej nei działa totalnie nie mam
    pomysłu jak zrobić odzielanie kropki więc próbowałem wyznaczać długośc i
    coś z tym kombinować....jutro rano zabieram sie by dokończyć ten program
    i następny zrobic

--- zadania z informatyki poprawa+string/2016_04_08_string_zad_1_dom.cpp
+++ zadania z informatyki poprawa+string/2016_04_08_string_zad_1_dom.cpp
@@ -17,24 +17,30 @@ int main()
 	int liczba_zdan=0,liczba_slow=0,srednia=0;
 	fstream zadanie;
 	zadanie.open("zaddom1.txt",ios::out);
-	if (plik.good())
+	if(zadanie.good())
 	{
+	
 		while(!zadanie.eof())
-		{
+		{		
+			int dlugosc;
+			dlugosc=zadanie.lenght();
+			char z;
+			z=zadanie.at(dlugosc);
+			cout<<"z = "<<z<<endl;
 			if(liczba_zdan!=1)
 			{
-				liczba_slow++
-				if(zdanie==.)
+				liczba_slow++;
+				if(zadanie==".")
 				{
 					liczba_zdan++;
 					cout<<"w zdaniu "<<liczba_zdan<<" jest "<<liczba_slow<<" slow."<<endl;
-					liczbaslow=0;
+					liczba_slow=0;
 				}
 			}
 			else
 			{
-				liczba_slow++
-				if(zdanie==.)
+				liczba_slow++;
+				if(zadanie==".")
 				{
 					liczba_zdan++;
 					cout<<"w zdaniu "<<liczba_zdan<<" jest "<<liczba_slow<<" slow."<<endl;
