023f208a0d038ea33fbb71d761f16fa2db6b29cc
mizami <kopeknumerouno@gmail.com>
2016-04-22 22:18:38 +0200

    experymentuje nad dokonczeniem programu
    próbuje dokonczyc to zadanei ale cały czas cos mi wyskakuje na problem

--- zadania z informatyki poprawa+string/2016_04_08_string_zad_2_dom.cpp
+++ zadania z informatyki poprawa+string/2016_04_08_string_zad_2_dom.cpp
@@ -4,22 +4,35 @@
 
 #include <iomanip>
 #include <iostream>
+#include <math.h>
 #include <fstream>
 
 using namespace std;
 
 int main()
 {
-	string liczba,zapas,liczba2;
+	string liczba,zapas,liczba2,mnozenie,wynik;//liczba-pocz¹tkowa liczba,liczba2-mno¿ymy przesz ni¹,mnozenie-pomnozenie przesz 1 cyfre z liczby,wynik-koncowy wynik
 	cout<<"podaj liczbe: ";
-	cin<<liczba;
+	cin>>liczba;
 	cout<<endl;
 	zapas=liczba;
-	liczba2=liczba
-	while(liczba>0)
-	{
-		(liczba%10)/=liczba2%10;
+	liczba2=liczba;
+	int ilosc=0,dlugosc=0,sterowanie=0,nowa_dlugosc=0;//ilosc-wartosc do zmian dlugosci  liczby2,dlugosc-wartosc do zmian dlugosci liczby,sterowanie-zlicza obroty pêtli,nowa dlugosc-zmienna dlugosci cyfry mnozonej 
+	ilosc=liczba2.length();//zliczam dlugosc cyfry2 by potem zmieniac jej wartosc
+	dlugosc=liczba.lenght();//zliczam dlugosc cyfry by zmieniac jej dlugosc
+	
+	while(ilosc>0)
+	{	
+		sterowanie++;
+		liczba.resize(dlugosc-sterowanie);//zmieniam dlugosc 1 liczby by miec 1 cyfre
+		nowa_dlugosc=ilosc-sterowanie;
+		liczba2.resize(nowa_dlugosc)
+		if (liczba==0)
+			liczba=zapas;
+		mnozenie=liczba*liczba2%10);
+		wynik+=mnozenie;
 	}
+	cout<<"wynik wynosi: "<<wynik<<endl;
 }
 
 
