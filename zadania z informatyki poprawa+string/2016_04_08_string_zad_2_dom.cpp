959b935c7048c17e1068f9737acd48118a25a667
mizami <kopeknumerouno@gmail.com>
2016-04-22 20:06:51 +0200

    to tylko początek potem na kartke sie przezuciłem

--- zadania z informatyki poprawa+string/2016_04_08_string_zad_2_dom.cpp
+++ zadania z informatyki poprawa+string/2016_04_08_string_zad_2_dom.cpp
@@ -2,4 +2,24 @@
 // nale¿y wykorzystaæ wskaŸniki i jeœli to mo¿liwe i sensowne utworzyæ osobne funkcje
 // wykorzystaæ mno¿enie pisemne
 
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	string liczba,zapas,liczba2;
+	cout<<"podaj liczbe: ";
+	cin<<liczba;
+	cout<<endl;
+	zapas=liczba;
+	liczba2=liczba
+	while(liczba>0)
+	{
+		(liczba%10)/=liczba2%10;
+	}
+}
+
 
